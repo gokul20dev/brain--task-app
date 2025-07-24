0dd2d0a903aa493b10a3c1d2955da38c20c2aedd
gokul <gopalakrishnan93843@gmail.com>
2025-07-24 15:08:35 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Set variables
-CLUSTER_NAME="brain-tasks-cluster"
+#CLUSTER_NAME="brain-tasks-cluster"
 REGION="ap-south-1"
 
 # Update kubeconfig to access EKS
 echo Configuring kubectl for EKS
-aws eks update-kubeconfig --region $REGION --name $CLUSTER_NAME 
+#aws eks update-kubeconfig --region $REGION --name $CLUSTER_NAME 
 
 # Apply Kubernetes manifests
 kubectl apply -f kubernetes/deployment.yml
